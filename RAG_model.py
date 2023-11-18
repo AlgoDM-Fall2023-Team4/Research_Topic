bfcbc19cf2428786b106768637129d55a0418370
jaiswal-arp <114616849+jaiswal-arp@users.noreply.github.com>
2023-11-17 21:42:00 -0500

    Update RAG_model.py
    Loading Data

--- RAG_model.py
+++ RAG_model.py
@@ -26,3 +26,17 @@ from langchain.embeddings.vertexai import VertexAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 import pandas as pd
 import json
+
+
+
+#Loading Data
+
+# Read the CSV file into a DataFrame
+csv_file_path = '/content/drive/MyDrive/output/Smart_Watch_Review.csv'
+df = pd.read_csv(csv_file_path)
+
+# Create a new text file and write the content to it
+txt_file_path = '/content/drive/MyDrive/output/output_file.txt'
+df.to_csv(txt_file_path, sep='\t', index=False)
+
+print(f'Text file created at: {txt_file_path}')
